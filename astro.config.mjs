a3300c7f3c4d275df96bda444b381d49f8e923ee
at946 <asato730@gmail.com>
2026-08-13 22:25:26 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://at946.github.io",
+  base: "/foooo",
+});
