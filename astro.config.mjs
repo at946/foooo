8cd93be90e6eeed1911f00e53d479a860bfcb2e9
at946 <asato730@gmail.com>
2026-08-19 22:26:22 +0900

    rename the service name to phewww

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://at946.github.io',
-  base: '/foooo',
+  base: '/phewww',
 
   vite: {
     plugins: [tailwindcss()],
